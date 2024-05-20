1834b227e91fe608ec3640c83ddce6e9ea2dc973
Steven-Mugisha <steven.mugishamizero@mail.mcgill.ca>
2024-05-19 22:37:55 -0400

    ComputerCanvas component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-screen mx-auto`}
+    >
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -23,7 +24,24 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-seconday flex justify-center items-start p-2'>
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            ></motion.dev>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
